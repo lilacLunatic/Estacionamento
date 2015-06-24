7300854fca9277e186289bc5b3961b3edd6f666f
asd <asd>
2015-06-24 14:24:41 -0300

    adicionado metodo Dao::dao_fetch_all

--- dao/Dao.php
+++ dao/Dao.php
@@ -12,6 +12,19 @@ class Dao{
 
 		return pg_fetch_array($result);
 	}
+
+	public function dao_fetch_all($query, $params=0){
+		$conexao = new Conexao();
+		$connection = $conexao->getConexao();
+		if($params){
+			$result = pg_query_params($conexao, $query, $params);
+		}else{
+			$result = pg_query($conexao, $query);
+		}
+		$conexao->closeConexao();
+
+		return pg_fetch_all($result);
+	}
 }
 
 ?>
