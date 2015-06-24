71f8ed789e271d2f949fc31aef20167984c10c9a
asd <asd>
2015-06-24 14:22:00 -0300

    adicionada classe Dao

---
+++ dao/Dao.php
@@ -0,0 +1,17 @@
+<?php
+class Dao{
+	public function dao_fetch_array($query, $params=0){
+		$conexao = new Conexao();
+		$connection = $conexao->getConexao();
+		if($params){
+			$result = pg_query_params($conexao, $query, $params);
+		}else{
+			$result = pg_query($conexao, $query);
+		}
+		$conexao->closeConexao();
+
+		return pg_fetch_array($result);
+	}
+}
+
+?>
