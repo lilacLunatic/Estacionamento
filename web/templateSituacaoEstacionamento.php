db489dfe6a593722d6f75a50d45fcae2eed5c435
Razue <lucas.viana.rs@hotmail.com>
2015-07-01 00:06:26 -0300

    Situação estacionamento:placeholder

---
+++ web/templateSituacaoEstacionamento.php
@@ -0,0 +1,19 @@
+<?php 
+
+	require_once("../lib/Template.php");
+    use raelgc\view\Template;
+    include "../dao/VeiculoDao.php";
+
+    $tpl = new Template("../view/cadastroVeiculos.html");
+    $veiculoDao = new VeiculoDao();
+    
+    $tipos = $veiculoDao->getTipos();
+    foreach($tipos as $t){
+        $tpl->OPTION = $t['id'];
+        $tpl->OPTION_NAME = $t['nome'];
+        $tpl->block("BLOCK_OPTIONS");
+    }
+
+    $tpl->show();
+
+?>
